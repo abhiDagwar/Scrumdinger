11b0a61e16baf662c2a894370ea0d5f6feedf09b
Abhishek Dagwar <47711239+abhiDagwar@users.noreply.github.com>
2025-04-15 12:38:58 +0530

    Initial commit: An iOS default template

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Abhishek G on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
