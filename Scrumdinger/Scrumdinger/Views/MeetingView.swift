514dbb6f01eb61413b726d0cad009883df5e2491
Abhishek Dagwar <47711239+abhiDagwar@users.noreply.github.com>
2025-04-16 17:14:53 +0530

    Added a zstack and roundedrectangle to meetingview

--- Scrumdinger/Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -8,46 +8,55 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.filled")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                ProgressView(value: 5, total: 15)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.tophalf.filled")
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.bottomhalf.filled")
+                    }
                 }
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
                 
-                Spacer()
+                Circle()
+                    .strokeBorder(lineWidth: 24)
                 
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.filled")
+                HStack {
+                    Text("Speaker 1 of 3”")
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
                 }
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
-            
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            
-            HStack {
-                Text("Speaker 1 of 3”")
-                
-                Spacer()
-                
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    @Previewable @State var scrum: DailyScrum = DailyScrum.sampleData[0]
+    MeetingView(scrum: $scrum)
 }
