a766c6f2025a28824623c4d59694a4eb6dab9899
Abhishek Dagwar <47711239+abhiDagwar@users.noreply.github.com>
2025-04-16 11:25:56 +0530

    Added detailView UI

---
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,46 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Abhishek G on 16/04/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                
+                HStack {
+                    Label("Timer", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
