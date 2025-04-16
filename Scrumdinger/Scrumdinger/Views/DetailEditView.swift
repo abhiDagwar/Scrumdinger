5aa2bad1be985dead1dc5623d263059baecc7d77
Abhishek Dagwar <47711239+abhiDagwar@users.noreply.github.com>
2025-04-16 16:08:54 +0530

    Added ThemePickerView and Pass a binding into the views

--- Scrumdinger/Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailEditView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    @State private var scrum: DailyScrum = DailyScrum.emptyScrum
+    @Binding var scrum: DailyScrum
     @State private var attendeeName = ""
     var body: some View {
         Form {
@@ -23,6 +23,7 @@ struct DetailEditView: View {
                     Text("\(scrum.lengthInMinutes) minutes")
                         .accessibilityHidden(true)
                 }
+                ThemePicker(selection: $scrum.theme)
             }
             
             Section(header: Text("Attenddes")) {
@@ -50,5 +51,6 @@ struct DetailEditView: View {
 }
 
 #Preview {
-    DetailEditView()
+    @Previewable @State var scrum = DailyScrum.sampleData[0]
+    DetailView(scrum: $scrum)
 }
